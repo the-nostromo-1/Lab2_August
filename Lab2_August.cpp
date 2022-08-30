82ad6df926eb8a1ff2529f7af5ae368c0ac37df2
tinma <tinma@DJT-LAPTOP>
2022-08-30 12:19:49 -0400

    Add project files.

---
+++ Lab2_August.cpp
@@ -0,0 +1,14 @@
+/*
+Program that takes numerical input and outputs the word for the number.
+i.e. 23 = "twenty three"
+David Tinley
+08/30/2022
+*/
+
+#include <iostream>
+
+using std::cin; using std::cout; using std::endl;
+
+int main() {
+
+}
