bbff451ebfb5138fe8d8807574c9096672c374fd
the-nostromo-1 <tinman11537@gmail.com>
2022-08-31 18:11:12 -0400

    more progress

--- Lab2_August.cpp
+++ Lab2_August.cpp
@@ -15,23 +15,74 @@ int main() {
 
 	cout << "Please enter a number between 1 and 31: " << endl;
 
-	cin >> x;
+	cin >> x; // user inputs a number that is stored in the x integer variable
 
-	int tens = x / 10;
-	int singles = x % 10;
+	int tens = x / 10; // extracts the digit that is in the "tenths" place of the # number entered
+	int singles = x % 10; // extracts the digit that is in the "ones or singles" place of the # number entered
 
 	// cout << tens << singles << endl;
 
 	switch (tens) {
 
-	case 0:
-		cout << "single digit";
-		break;
+		//case 0:
+			//cout << "single digit";
+			//break;
 	case 1:
 		cout << "Ten";
 		break;
 	case 2:
 		cout << "Twenty";
 		break;
+
+	case 3:
+		cout << "Thirty";
+		break;
 	}
+
+	switch (singles) {
+
+	case 0:
+		cout << "";
+		break;
+
+	case 1:
+		cout << "One";
+		break;
+
+	case 2:
+		cout << "Two";
+		break;
+
+	case 3:
+		cout << "Three";
+		break;
+
+	case 4:
+		cout << "Four";
+		break;
+
+	case 5:
+		cout << "Five";
+		break;
+
+	case 6:
+		cout << "Six";
+		break;
+
+	case 7:
+		cout << "Seven";
+		break;
+
+	case 8:
+		cout << "Eight";
+		break;
+
+	case 9:
+		cout << "Nine";
+		break;
+
+	default:
+		cout << "Please enter a number between 1 and 31.";
+	}
+
 }
