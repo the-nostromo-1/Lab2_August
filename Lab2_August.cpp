064545d2b64518f29bd442584e76dad9947da4ec
the-nostromo-1 <tinman11537@gmail.com>
2022-08-31 13:54:53 -0400

    update 1

--- Lab2_August.cpp
+++ Lab2_August.cpp
@@ -11,4 +11,26 @@ using std::cin; using std::cout; using std::endl;
 
 int main() {
 
+	int x;
+
+	cout << "Please enter a number between 1 and 31: " << endl;
+
+	cin >> x;
+
+	int tens = x / 10;
+	int singles = x % 10;
+
+	// cout << tens << singles << endl;
+
+	switch (tens) {
+
+	case 0:
+		break;
+
+	case 1:
+		cout << "Ten";
+		break;
+
+	}
+
 }
